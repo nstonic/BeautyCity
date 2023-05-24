1e1557d640450804f23542b00062a3f26b9e669a
fch-yk <yefim_korshever@mail.ru>
2023-05-24 22:02:15 +0300

    add services to index template

--- services/views.py
+++ services/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from .models import Salon, Service
 
 
 class Index(TemplateView):
@@ -9,22 +10,7 @@ class Index(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        salons = [
-            {
-                'name': 'Beauty city Пушкинская',
-                'address': 'ул. Пушкинская, д. 78А',
-                'photo': {'url': 'img/salons/salon1.svg'},
-            },
-            {
-                'name': 'Beauty city Ленина',
-                'address': 'ул. Ленина, д. 211',
-                'photo': {'url': 'img/salons/salon2.svg'},
-            },
-            {
-                'name': 'Beauty city Московская',
-                'address': 'ул. Московская, д. 67',
-                'photo': {'url': 'img/salons/salon3.svg'},
-            },
-        ]
-        context['salons'] = salons
+        context['salons'] = Salon.objects.all()
+        context['services'] = Service.objects.all()
+
         return context
