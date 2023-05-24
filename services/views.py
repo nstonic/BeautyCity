c7902747ab80eb3bde273f905e21644165cb5f7f
fch-yk <yefim_korshever@mail.ru>
2023-05-24 23:41:59 +0300

    Add masters to the index template

--- services/views.py
+++ services/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 from .models import Salon, Service
+from users.models import Master
+from django.utils.timezone import localdate
+from dateutil import relativedelta
 
 
 class Index(TemplateView):
@@ -12,5 +15,17 @@ class Index(TemplateView):
         context = super().get_context_data(**kwargs)
         context['salons'] = Salon.objects.all()
         context['services'] = Service.objects.all()
+
+        masters = Master.objects.all()
+        now = localdate()
+        for master in masters:
+            delta = relativedelta.relativedelta(now, master.experience_from)
+            years = months = ''
+            if delta.years:
+                years = f'{delta.years} г.'
+            if delta.months:
+                months = f'{delta.months} мес.'
+            master.experience = f'{years} {months}'
+        context['masters'] = masters
 
         return context
