0806fa244ca896b6aebdfc91a0ae3041cabfbdc2
ZHartem21 <zhartem21@gmail.com>
2023-05-24 11:59:11 +0300

    Add model and admin settings

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Service, Salon
 
-# Register your models here.
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+    )
+
+
+@admin.register(Salon)
+class SalonAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'address',
+    )
