34f65ebbd7c3bb951598bfa875ce8e3138310cb9
nstonic <118763806+nstonic@users.noreply.github.com>
2023-05-25 23:57:36 +0400

    Make order by master_id

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,6 @@ from orders.views import MakeOrder, order_final
 
 urlpatterns = [
     path('configure', MakeOrder.as_view(), name='configure_order'),
+    path('configure/<int:master_id>', MakeOrder.as_view(), name='master_order'),
     path('final/<int:order_id>', order_final, name='final_order')
 ]
--- orders/views.py
+++ orders/views.py
@@ -16,7 +16,6 @@ class MakeOrder(TemplateView):
     template_name = 'service.html'
 
     def get_context_data(self, **kwargs):
-
         context = super().get_context_data(**kwargs)
         context.update({
             'masters': Master.objects.all(),
@@ -24,6 +23,10 @@ class MakeOrder(TemplateView):
             'services': Service.objects.all(),
             'orders': Order.objects.all()
         })
+        if master_id := kwargs.get('master_id'):
+            context.update({
+                'master': Master.objects.filter(pk=master_id).first()
+            })
         return context
 
     def post(self, request, *args, **kwargs):
