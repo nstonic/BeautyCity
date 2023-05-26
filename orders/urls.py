e76332144d9f3602a3ed931caf9db82e2e4238af
nstonic <nstonic@gmail.com>
2023-05-26 13:39:45 +0400

    Create class based view

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from orders.views import MakeOrder, order_final
+from orders.views import MakeOrder, OrderFinally
 
 urlpatterns = [
     path('configure', MakeOrder.as_view(), name='configure_order'),
     path('configure/<int:master_id>', MakeOrder.as_view(), name='master_order'),
-    path('final/<int:order_id>', order_final, name='final_order')
+    path('final/<int:order_id>', OrderFinally.as_view(), name='final_order')
 ]
