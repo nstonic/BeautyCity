33154f73e1c49b590d198fccee3e79fd2797c8d9
fch-yk <yefim_korshever@mail.ru>
2023-05-27 15:02:11 +0300

    Create/Update the client and update the order

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from orders.views import MakeOrder, OrderFinally
+from orders.views import MakeOrder, OrderFinally, AcceptedOrder
 
 urlpatterns = [
     path('configure/', MakeOrder.as_view(), name='configure_order'),
-    path('configure/<int:master_id>/', MakeOrder.as_view(), name='master_order'),
-    path('final/<int:order_id>/', OrderFinally.as_view(), name='final_order')
+    path(
+        'configure/<int:master_id>/',
+        MakeOrder.as_view(),
+        name='master_order'
+    ),
+    path('final/<int:order_id>/', OrderFinally.as_view(), name='final_order'),
+    path(
+        'accepted_order/<int:order_id>/',
+        AcceptedOrder.as_view(),
+        name='accepted_order'
+    ),
 ]
